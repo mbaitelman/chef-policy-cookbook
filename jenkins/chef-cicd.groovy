b87f39156833d4c68930b378b2e2f07a2d524834
Mendy Baitelman <mendyb@prodege.com>
2020-04-07 15:54:09 -0700

    Add CICD job

---
+++ jenkins/chef-cicd.groovy
@@ -0,0 +1,29 @@
+pipeline {
+    agent none
+
+    options {
+        disableConcurrentBuilds()
+        timestamps()
+        timeout(time: 1, unit: 'HOURS')
+    }
+
+    stages {
+        stage('Run TestKitchen') {
+            steps {
+                build job: 'TestKitchen', parameters: [string(name: 'POLICY_NAME', value: ''), string(name: 'COOKBOOK', value: 'mbaitelman/chef-policy-cookbook'), string(name: 'BRANCH', value: 'master')]
+            }
+        }
+        stage('Create Policy Archives'){
+            steps {
+                script {
+                    def uploadJobResult = build job: 'UploadPolicies', parameters: [string(name: 'BRANCH', value: 'master'), string(name: 'COOKBOOK', value: 'mbaitelman/chef-policy-cookbook')]
+                }
+            }
+        }
+        stage('Push Archive Files') {
+            steps {
+                build job: 'PushPolicies', parameters: [string(name: 'POLICY_GROUP', value: 'qa'), string(name: 'POLICY_NAME', value: ''), string(name: 'BUILD_REVISION', value: uploadJobResult.getNumber()), string(name: 'COOKBOOK', value: 'mbaitelman/chef-policy-cookbook')]
+            }
+        }
+    }
+}
