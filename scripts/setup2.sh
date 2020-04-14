83db79ffae0142a3ae8f820b42893b8b973a8622
Mendy Baitelman <mendyb@prodege.com>
2020-04-13 20:44:52 -0700

    Wait a little longer

--- scripts/setup2.sh
+++ scripts/setup2.sh
@@ -1,4 +1,4 @@
 docker-compose up -d
-sleep 20
+sleep 40
 # Update artifactory 
 curl -uadmin:password -X PATCH "http://localhost:8081/artifactory/api/system/configuration" -H "Content-Type:application/yaml" -T artifactory/configuration.yml
